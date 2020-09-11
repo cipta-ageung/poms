73a3af852922eda3c1bd8f00cd61d0bb64c713fb
ciptaageung <cipta_ageung@yahoo.co.id>
2020-09-12 00:32:03 +0700

    add migrations checklist

--- database/migrations/2019_11_14_105120_create_check_list_table.php
+++ database/migrations/2019_11_14_105120_create_check_list_table.php
@@ -19,6 +19,7 @@ class CreateCheckListTable extends Migration
             $table->integer('task_id')->default('0');
             $table->integer('created_by')->default('0');
             $table->integer('status')->default('0');
+            $table->integer('percentage')->default('0');
             $table->timestamps();
         });
     }
